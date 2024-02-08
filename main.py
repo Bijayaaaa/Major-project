436adacda09d7e4321223dc59ec2ceb2f3dedae2
Bijayaaaa <bijayabhandari2057@gmail.com>
2024-02-08 16:04:23 +0545

    myproject

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from servo_control import initialize_servos, get_camera_servo_angle, rotate_servo
 from pump_control import run_pump
-
+from model import class_index
 # Define GPIO pins for pumps
 PUMP_1_PIN = 22
 PUMP_2_PIN = 24
@@ -17,3 +17,6 @@ pi = initialize_servos(CAMERA_SERVO_PIN, NOZZLE_SERVO_PIN)
 # Move the camera servo to a 45-degree angle
 target_angle = 45
 rotate_servo(pi, CAMERA_SERVO_PIN, target_angle)
+
+capture_image
+
